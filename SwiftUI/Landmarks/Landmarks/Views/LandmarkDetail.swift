022ff5482acef9951232ae3181353e520a6b1429
Yohan Avishke <yaediriweera@gmail.com>
2021-02-26 23:59:26 +0530

    Setup navigation

---
+++ SwiftUI/Landmarks/Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,54 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Yohan Avishke Ediriweera on 2021-02-26.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        ScrollView {
+            
+            VStack {
+                MapView()
+                    .ignoresSafeArea(edges: .top)
+                    .frame(height: 300)
+                
+                CircleImage()
+                    .offset(y: -130)
+                    .padding(.bottom, -130)
+                
+                VStack(alignment: .leading) {
+                    Text("Turtle Rock")
+                        .font(.title)
+                    
+                    HStack {
+                        Text("Joshua Tree National Park")
+                        Spacer()
+                        Text("California")
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                    
+                    Divider()
+                    
+                    Text("About Turtle Rock")
+                        .font(.title2)
+                    Text("Descriptive text goes here.")
+                }
+                .padding()
+                
+                Spacer()
+            }
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
