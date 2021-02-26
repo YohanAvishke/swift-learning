b588404c3b7ed1484fb0f881d059b587eabf8524
Yohan Avishke <yaediriweera@gmail.com>
2021-02-26 21:19:28 +0530

    first commit

---
+++ SwiftUI/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Yohan Avishke Ediriweera on 2021-02-26.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
